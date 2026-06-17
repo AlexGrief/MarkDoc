004491525001145c4f0774d791e2ace414d5c281
MountCD <mountcd@proton.me>
2026-06-17 12:21:36 +0700

    Make paragraph part in match structure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, fs::File, io::Write};
 
-use docx_rs::{DocumentChild, Paragraph, read_docx};
+use docx_rs::{DocumentChild, Paragraph, ParagraphChild, RunChild, read_docx};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input: String = env::args().collect();
@@ -13,8 +13,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     for child in &docx.document.children {
         match child {
-            DocumentChild::Paragraph(p) => {
-                todo!()
+            DocumentChild::Paragraph(paragraph) => {
+                let text = extract_paragraph_text(paragraph);
+
+                if let Some(style) = &paragraph.property.style {
+                    match style.val.as_str() {
+                        "Heading1" => md_writer.write_header(1, &text),
+                        "Heading2" => md_writer.write_header(2, &text),
+                        "Heading3" => md_writer.write_header(3, &text),
+                        _ => md_writer.write_paragraph(&text), // Пишем все что не попало в фильтр как обычный текст
+                    }
+                } else {
+                    md_writer.write_paragraph(&text);
+                }
             }
             DocumentChild::Table(table) => todo!(),
             DocumentChild::BookmarkStart(bookmark_start) => todo!(),
@@ -59,4 +70,31 @@ impl MarkdownWriter {
         output.write_all(self.buffer.as_bytes())?;
         Ok(())
     }
+}
+
+fn extract_paragraph_text(paragraph: &docx_rs::Paragraph) -> String {
+    let mut paragraph_text = String::new();
+
+    for parchild in &paragraph.children {
+        if let ParagraphChild::Run(run) = parchild {
+            let mut text = String::new();
+
+            for run_child in &run.children {
+                if let RunChild::Text(t) = run_child {
+                    text.push_str(&t.text);
+                }
+            }
+
+            if run.run_property.bold.is_some() {
+                text = format!("**{}**", text)
+            }
+
+            if run.run_property.italic.is_some() {
+                text = format!("*{}*", text)
+            }
+
+            paragraph_text.push_str(&text);
+        }
+    }
+    paragraph_text
 }
